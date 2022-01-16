96eab358fa6662d6248613e61257f6128202fd29
LDK <lemondropkid@gmail.com>
2022-01-15 17:00:21 -0800

    Notifications and responsive keyboard

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-// import validWords from "./validWords.json";
+import validWords from "./validWords.json";
 import answers from "./answers.json";
 
 const LetterSquare = (props) => {
@@ -24,26 +24,39 @@ const LetterRow = (props) => {
   let end = offset + wordLength - 1;
   let squares = [];
   let i = offset;
+  let lettersSeen = {};
   while (i <= end) {
-	  let result = null;
-	  if (guesses[i] && sent) {
-		  if (guesses[i] === answer[i - offset]) {
-			result = 'right-spot';
-		  }
-		  else if (answer.indexOf(guesses[i]) !== -1) {
-			  result = 'wrong-spot';
-		  }
-		  else {
+    let result = null;
+	const gLtr = guesses[i];
+	if (!lettersSeen[gLtr]) {
+		lettersSeen[gLtr] = 1;
+	}
+	else {
+		lettersSeen[gLtr]++;
+	}
+    if (gLtr && sent) {
+      if (gLtr === answer[i - offset]) {
+		  result = 'right-spot';
+      }
+      else if (answer.indexOf(gLtr) !== -1) {
+		  if (lettersSeen[gLtr] > letterCounts[gLtr]) {
 			  result = 'no-spot';
 		  }
-	  }
-	  squares.push(<LetterSquare letter={guesses[i]} result={result} active={active === i && !victory} />);
-	  i++;
+		  else {
+			  result = 'wrong-spot';
+		  }
+      }
+      else {
+        result = 'no-spot';
+      }
+    }
+    squares.push(<LetterSquare letter={guesses[i]} result={result} active={active === i && !victory} />);
+    i++;
   }  
   return (
-  	<Row>
-	  { squares }
-	  </Row>
+    <Row>
+    { squares }
+    </Row>
   );
 };
 
@@ -53,115 +66,210 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 };
 
+const wordLength = 5;
+const guessCount = 6;
 const picked = answers[getRandomInt(0,answers.length)].split('');
 
-function App() {
-  const wordLength = 5;
-  const guessCount = 6;
-  // const [answer, setAnswer] = useState(['U','N','C','L','E']);
+let wordIndex = 0;
+let letterCounts = {};
+while (wordIndex < wordLength) {
+	let ltr = picked[wordIndex];
+	if (letterCounts[ltr]) {
+		letterCounts[ltr]++
+	}
+	else {
+		letterCounts[ltr] = 1;
+	}
+	wordIndex++;
+}
+
+const App = () => {
   const [answer, setAnswer] = useState(picked);
   const [guesses, setGuesses] = useState([]);
   const [guessesUsed, setGuessesUsed] = useState(0);
   const [victory, setVictory] = useState(false);
   const [failure, setFailure] = useState(false);
+  const [notificationVisible, setNotificationVisible] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState(false);
   const [resultMessage, setResultMessage] = useState(null);
+  const [lettersGuessed, setLettersGuessed] = useState({});
   const endOfLine = guesses.length && (guesses.length % wordLength === 0) && guessesUsed < Math.floor(guesses.length / wordLength);
   const startOfLine = !guesses.length || ((guesses.length % wordLength === 0) && guessesUsed >= Math.floor(guesses.length / wordLength));
+  console.log('end?',endOfLine,'start?',startOfLine,'guesses',guesses,'guessesUsed',guessesUsed);
+
+  const Notification = (props) => {
+  	const { timeOut } = props;
+	if (notificationVisible) {
+		setTimeout(() => { if (notificationVisible) { setNotificationVisible(false); } }, timeOut);
+	}
+	return (
+		<div className={ "notification " + (notificationVisible ? 'opacity-100' : 'opacity-0') }>{ notificationMessage }</div>
+	);
+  }
+
   let letterRows = [];
   let i = 0;
   while (i < guessCount) {
-	  letterRows.push(
+    letterRows.push(
         <LetterRow rowNum={i} guesses={guesses} answer={answer} wordLength={wordLength} active={guesses.length - (endOfLine ? 1 : 0)} sent={guessesUsed > i} victory={victory} />
-	  );
-	  i++;
+    );
+    i++;
   }
   const handleLetter = (ltr) => {
-	  setGuesses(guesses => [...guesses, ltr]);
+	if (notificationVisible) { 
+		setNotificationVisible(false); 
+	}
+    setGuesses(guesses => [...guesses, ltr]);
   };
   const handleBackSpace = () => {
-	  if (startOfLine) {
-		  return;
-	  }
-	  let newInput = [...guesses];
-	  newInput.pop();
-	  setGuesses(newInput);
+    if (startOfLine) {
+      return;
+    }
+    let newInput = [...guesses];
+    newInput.pop();
+    setGuesses(newInput);
   };
   const checkVictory = () => {
-	  let i = 0;
-	  let j = 0;
-	  while (i < guesses.length) {
-		  let guess = guesses.slice(i, i + wordLength);
-		  console.log('guesses',guesses,'guess',guess);
-		  i += wordLength;
-		  j++;
-		  if (answer.join() === guess.join()) {
-			  setVictory(true);
-			  setResultMessage(<p className="result-message">You got the answer in {guessesUsed} tries!  Press Enter for a new word.</p>);
-		  }
-		  else if (j >= guessCount) {
-			  setFailure(true);
-			  setResultMessage(<p className="result-message">Sorry, the answer was "{answer.join('')}."  Press Enter for a new word.</p>);
+    let i = 0;
+    let j = 0;
+	let newLettersGuessed = {};
+	let lettersSeen = {};
+    while (i < guesses.length) {
+      let guess = guesses.slice(i, i + wordLength);
+	  let guessWord = guess.join('').toLowerCase();
+	  if (validWords.indexOf(guessWord) === -1) {
+		  console.log('Invalid word',guessWord);
+		  let newGuesses = [...guesses].slice(0,i);
+		  setGuesses(newGuesses);
+		  setGuessesUsed(newGuesses.length / wordLength);
+		  console.log('oi',newGuesses.length / wordLength,guessesUsed);
+		  setNotificationMessage(guessWord.toUpperCase() + " is not a valid word.");
+		  setNotificationVisible(true);
+		  return;
+	  }
+      setGuessesUsed(guessesUsed + 1);
+      i += wordLength;
+      j++;
+	  for (let guessIdx in guess) {
+		  let gLtr = guess[guessIdx];
+		  if (!lettersSeen[gLtr]) {
+			  lettersSeen[gLtr] = 1;
 		  }
 		  else {
-			  console.log('Incorrect guess',guess.join(),'Guesses used',j);
+			  lettersSeen[gLtr]++;
+		  }
+		  if (answer.indexOf(gLtr) === -1) {
+			  newLettersGuessed[gLtr] = 'none';
+		  }
+		  else if (answer[guessIdx] === gLtr) {
+			  newLettersGuessed[gLtr] = 'right';
+		  }
+		  else if (!newLettersGuessed[gLtr]) {
+		 	  newLettersGuessed[gLtr] = 'wrong';
 		  }
 	  }
-	  
+	  setLettersGuessed(newLettersGuessed);
+      if (answer.join() === guess.join()) {
+        setVictory(true);
+        setResultMessage(<p className="result-message">You got the answer in {guessesUsed + 1} tries!  Press Enter for a new word.</p>);
+      }
+      else if (j >= guessCount) {
+        setFailure(true);
+        setResultMessage(<p className="result-message">Sorry, the answer was "{answer.join('')}."  Press Enter for a new word.</p>);
+      }
+      else {
+        console.log('Incorrect guess',guess.join(),'Guesses used',j);
+      }
+    }
+    
   };
   const handleEnter = () => {
-	  if (!endOfLine) {
-	  	// TODO: Send "not enough letters message"
-		  return;
-	  }
-	  setGuessesUsed(guessesUsed + 1);
-	  checkVictory();
+    if (!endOfLine) {
+		setNotificationMessage("Not enough letters.");
+		setNotificationVisible(true);
+		return;
+    }
+    checkVictory();
+	console.log('letters guessed for',answer,lettersGuessed);
   };
   const handleKeyDown = (key) => {
-	  if (key === 'Enter') {
-		  if (victory || failure) {
-			  newGame();
-			  return;
-		  }
-		  handleEnter();
-		  return;
-	  }
-	  if (victory) {
-		  return;
-	  }
-	  const isLetter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(key) !== -1;
-	  if (isLetter && !endOfLine) {
-		  handleLetter(key.toUpperCase());
-		  return;
-	  }
-	  if (key === 'Backspace') {
-		  handleBackSpace();
-		  return;
-	  }
+    if (key === 'Enter') {
+      if (victory || failure) {
+        newGame();
+        return;
+      }
+      handleEnter();
+      return;
+    }
+    if (victory) {
+      return;
+    }
+    const isLetter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(key) !== -1;
+    if (isLetter && !endOfLine) {
+      handleLetter(key.toUpperCase());
+      return;
+    }
+    if (key === 'Backspace') {
+      handleBackSpace();
+      return;
+    }
   };
   const newGame = () => {
-  	setVictory(false);
-	setGuesses([]);
-	setGuessesUsed(0);
-	setAnswer(answers[getRandomInt(0,answers.length)].split(''));
-	setResultMessage(null);
+    setVictory(false);
+	setFailure(false);
+  setGuesses([]);
+  setGuessesUsed(0);
+  setAnswer(answers[getRandomInt(0,answers.length)].split(''));
+  setResultMessage(null);
+  setLettersGuessed({});
   };
+	const keyRows = [
+		['Q','W','E','R','T','Y','U','I','O','P',{ key: 'Backspace', label: '← Bk', className: 'd-none d-sm-inline-block actionKey' },{ key: 'Backspace', label: '←', className: 'd-sm-none actionKey' }],
+		['A','S','D','F','G','H','J','K','L'],
+		['Z','X','C','V','B','N','M',{ key: 'Enter', label: 'Enter ⏎', className: 'd-none d-sm-inline-block actionKey' },{ key: 'Enter', label: '⏎', className: 'd-sm-none actionKey' }]
+	];
+	let keyBtnRows = [];
+	for (var k in keyRows) {
+		let myRow = [];
+		for (var keyIdx in keyRows[k]) {
+			let label = keyRows[k][keyIdx];
+			let keyVal = label;
+			let classes = '';
+			if (typeof label == 'object') {
+				keyVal = label.key;
+				classes = label.className;
+				label = label.label;
+			}
+			myRow.push(<div className={"py-1 px-1 px-sm-2 mx-1 mb-2 keyButton d-inline-block " + classes} data-result={lettersGuessed[label] || 'unguessed'} style={{ color: '#333', border: '#ACACAC', borderRadius: '.25rem' }} onClick={ () => { handleKeyDown(keyVal) } }>{ label }</div>);
+		}
+		keyBtnRows.push(
+			<div className="pt-2">{ myRow }</div>
+		)
+	}
   return (
-	  <Container fluid={true} tabIndex={1} onKeyDown={(e) => { handleKeyDown(e.key); }} className="h-100"> 
-		<Container className="App" style={{ maxWidth: '420px', backgroundColor: '#003300' }}>
-	    	<Row>
-			  <Col xs={12}>
-				  { letterRows }
-			  </Col>
-			</Row>
-		  { resultMessage &&
-		  	<Row>
-			  <Col xs={12} className="p-0">
-			  { resultMessage }
-			  </Col>
-			  </Row>
-		  }
-		</Container>
-	</Container>
+    <Container fluid="sm" tabIndex={1} onKeyDown={(e) => { handleKeyDown(e.key); }} className="h-100 text-center pt-4 px-0 px-sm-4"> 
+	  <h3 className="mt-md-4 mt-lg-5">A <a href="https://www.powerlanguage.co.uk/wordle/">Wordle</a> clone built in React</h3>
+	  <p className="mt-2 mb-0">By Daniel Swinney</p>
+	  <a className="d-block mt-0 mb-4 small" href="https://github.com/LDK/react-wordle/">GitHub repo</a>
+	    <Container className="App" style={{ maxWidth: '480px', backgroundColor: '#003300' }}>
+	        <Row>
+	        <Col xs={12}>
+	          { letterRows }
+	        </Col>
+	      </Row>
+	      <Row className="onscreenKeyboard">
+			  { keyBtnRows }
+	      </Row>
+		  <Notification timeOut={2500} />
+	      { resultMessage &&
+	      <Row>
+	        <Col xs={12} className="p-0">
+	        { resultMessage }
+	        </Col>
+	      </Row>
+	      }
+	    </Container>
+  </Container>
   );
 }
 
